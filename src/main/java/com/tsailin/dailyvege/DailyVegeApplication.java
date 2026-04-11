7dcc50a7d721ac248e71306579908ee12c4a2013
tsailin <swchloetl@gmail.com>
2026-04-11 18:16:52 +0900

    init

---
+++ src/main/java/com/tsailin/dailyvege/DailyVegeApplication.java
@@ -0,0 +1,13 @@
+package com.tsailin.dailyvege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyVegeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyVegeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsailin/dailyvege/DailyVegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsailin.dailyvege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyVegeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
